186ddbbf9732108c794bb27b9e2caa4cd923041d
Boloutare Doubeni <tarecd@gmail.com>
2016-02-29 12:37:09 -0500

    Added Data Binding

--- app/src/main/java/com/farazfazli/oscar/MainActivity.java
+++ app/src/main/java/com/farazfazli/oscar/MainActivity.java
@@ -1,13 +1,27 @@
 package com.farazfazli.oscar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.oscar_nominees);
+        ArrayList<OscarWinner> winners = new ArrayList<OscarWinner>() {{
+            add(new OscarWinner("Leo", "1974", "1"));
+            add(new OscarWinner("Matt Damon", "1973", "1"));
+        }};
+
+        OscarWinnersAdapter adapter  = new OscarWinnersAdapter(this, winners);
+        mListView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/farazfazli/oscar/OscarWinnersAdapter.java
+++ app/src/main/java/com/farazfazli/oscar/OscarWinnersAdapter.java
@@ -1,11 +1,13 @@
 package com.farazfazli.oscar;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.TextView;
+
+import com.farazfazli.oscar.databinding.ItemOscarWinnerBinding;
 
 import java.util.ArrayList;
 
@@ -13,8 +15,10 @@ import java.util.ArrayList;
  * Created by faraz on 2/29/16.
  */
 public class OscarWinnersAdapter extends ArrayAdapter<OscarWinner> {
+    private Context mContext;
     public OscarWinnersAdapter(Context context, ArrayList<OscarWinner> oscarWinnersArrayList) {
         super(context, 0, oscarWinnersArrayList);
+        mContext = context;
     }
 
     @Override
@@ -22,18 +26,10 @@ public class OscarWinnersAdapter extends ArrayAdapter<OscarWinner> {
         // Get the data item for this position
         OscarWinner oscarWinner = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_oscar_winner, parent, false);
-        }
+
+        ItemOscarWinnerBinding binding = DataBindingUtil.inflate(LayoutInflater.from(mContext), R.layout.item_oscar_winner, parent, false);
         // Lookup view for data population
-        TextView mNameTextView = (TextView) convertView.findViewById(R.id.name);
-        TextView mDOBTextView = (TextView) convertView.findViewById(R.id.dob);
-        TextView mOscarsTextView = (TextView) convertView.findViewById(R.id.oscars);
-        // Populate the data into the template view using the data object
-        mNameTextView.setText(oscarWinner.getName());
-        mDOBTextView.setText(oscarWinner.getDOB());
-        mOscarsTextView.setText(oscarWinner.getOscarsWon());
-        // Return the completed view to render on screen
-        return convertView;
+        binding.setWinner(oscarWinner);
+        return binding.getRoot();
     }
 }
